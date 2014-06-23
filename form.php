e2d4fda0e7e3b004a8fd4c43d0719cac47619c26
assitaliroma <assitaliroma@gmail.com>
2014-06-23 10:28:19 -0430

    Drag and Drop example + new form controls

---
+++ form.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>
+		Recepcion de data en el lado del servidor
+		</title>
+	</head>
+	<body>
+		<h1>
+		Leer datos enviados por el cliente
+		</h1>
+		<?php
+		echo $_REQUEST["consulta"];
+		?>
+	</body>
+</html>
